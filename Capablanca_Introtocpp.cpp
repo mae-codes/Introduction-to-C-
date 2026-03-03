eac57027aa09aa9c210772b893022b59d1ed7fbb
Dimple Ann <dimpleannmae@gmail.com>
2026-03-03 22:11:43 +0800

    Add files via upload

---
+++ Capablanca_Introtocpp.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // declare int variables named length and width
+    int length;
+    int width;
+
+    
+    cout << "=================================================\n" ;
+    cout << "Dimple Ann Mae S. Capablanca, BSIT 1.3, 425002521\n";
+    cout << "=================================================\n" ;
+    
+    // the values of length and width come from the user
+    cout << "Length : "; 
+    cin >> length;
+    cout << "Width : ";
+    cin >> width;
+
+int area;
+area = length * width;
+  cout << "Area :" << area << endl;
+  
+    return 0;
+}
